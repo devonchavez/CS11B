e264d2cf08da5fd2537ac29ba3af03b586a113b0
Devon Chavez <ddevc415@gmail.com>
2025-06-16 20:39:57 -0700

    Lab1 Mod2

--- LabProgram.java
+++ LabProgram.java
@@ -1,5 +1,38 @@
+/*
+Name: Devon Chavez
+Course: CS 111B Programming Fundamentals: Java
+Instructor: Dave Harden
+Date: June 16, 2025
+File Name: LabProgram.Java
+
+The program will take 2 numbers from the user and display the 1st number divided by the 2nd number 3 times.
+Input:
+2000 2
+Output:
+1000 500 250
+ */
+
+import java.util.Scanner;
+
 public class LabProgram {
+    //main method
     public static void main(String[] args) {
-        System.out.println("hello world");
+        //input variables
+        Scanner input = new Scanner(System.in);
+        int userNum;
+        int divNum;
+
+        //convert input to ints
+        userNum = input.nextInt();
+        divNum = input.nextInt();
+
+        //calculations & display output
+        userNum = userNum / divNum;
+        System.out.println(userNum);
+        userNum = userNum / divNum;
+        System.out.println(userNum);
+        userNum = userNum / divNum;
+        System.out.println(userNum);
+
     }
 }
