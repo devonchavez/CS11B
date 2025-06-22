44cc6fd1e25eb09504c928e7f4e0591dd022f28c
Devon Chavez <ddevc415@gmail.com>
2025-06-21 22:02:19 -0700

    Lab2 Mod3

--- LabProgram.java
+++ LabProgram.java
@@ -5,11 +5,32 @@ Instructor: Dave Harden
 Date: June 21, 2025
 File Name: LabProgram.Java
 
-The program takes 3 ints from the user and displays smallest int
-Input:
-7 15 3
-Output:
-3
+The program takes in the users interstate highway numbers and displays which one theyre taking and which direction its going
+Ex: If the input is:
+
+90
+the output is:
+
+I-90 is primary, going east/west.
+Ex: If the input is:
+
+290
+the output is:
+
+I-290 is auxiliary, serving I-90, going east/west.
+Ex: If the input is:
+
+0
+or any number not between 1 and 999, the output is:
+
+0 is not a valid interstate highway number.
+Ex: If the input is:
+
+200
+the output is:
+
+200 is not a valid interstate highway number.
+
 */
 
 import java.util.Scanner;
@@ -21,18 +42,32 @@ public class LabProgram {
         Scanner input = new Scanner(System.in);
 
         //input swaps to ints
-        int num1 = input.nextInt();
-        int num2 = input.nextInt();
-        int num3 = input.nextInt();
+        int highwayNumber = input.nextInt();
 
-        //check and display smallest int
-        if (num1 < num2 && num1 < num3){
-            System.out.println(num1);
-        } else if (num2 < num3 && num2 < num1) {
-            System.out.println(num2);
+        //invalid highway numbers
+        if (highwayNumber < 1 || highwayNumber > 999) {
+            System.out.println(highwayNumber + " is not a valid interstate highway number.");
+        } else if (highwayNumber >= 1 && highwayNumber <= 99) {
+            // Primary highway
+            if (highwayNumber % 2 == 0) {
+                System.out.println("I-" + highwayNumber + " is primary, going east/west.");
+            } else {
+                System.out.println("I-" + highwayNumber + " is primary, going north/south.");
+            }
         } else {
-            System.out.println(num3);
-        }
+            // Auxiliary highway
+            int primaryHighway = highwayNumber % 100;
 
+            if (primaryHighway == 0) {
+                System.out.println(highwayNumber + " is not a valid interstate highway number.");
+            } else {
+
+                if (primaryHighway % 2 == 0) {
+                    System.out.println("I-" + highwayNumber + " is auxiliary, serving I-" + primaryHighway + ", going east/west.");
+                } else {
+                    System.out.println("I-" + highwayNumber + " is auxiliary, serving I-" + primaryHighway + ", going north/south.");
+                }
+            }
+        }
     }
 }
