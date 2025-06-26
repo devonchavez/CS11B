14600d5ee51c8c812ec77fff2f2c6e189d269420
Devon Chavez <ddevc415@gmail.com>
2025-06-26 01:43:50 -0700

    Lab3 Mod4

--- LabProgram.java
+++ LabProgram.java
@@ -5,35 +5,13 @@ Instructor: Dave Harden
 Date: June 26, 2025
 File Name: LabProgram.Java
 
-This program will count how many characters are in a string the user inputs.
+This program will read string input from the user and remove and non alpha numbers from the string.
 
 Ex: If the input is:
+-Hello, 1 world$!
 
-n Monday
 the output is:
-
-1 n
-Ex: If the input is:
-
-z Today is Monday
-the output is:
-
-0 z's
-Ex: If the input is:
-
-n It's a sunny day
-the output is:
-
-2 n's
-Case matters.
-
-Ex: If the input is:
-
-n Nobody
-the output is:
-
-0 n's
-n is different than N.
+Helloworld
 */
 
 import java.util.Scanner;
@@ -43,23 +21,16 @@ public class LabProgram {
     public static void main(String[] args) {
         //scanner and variables
         Scanner scnr = new Scanner(System.in);
-        char userChar = scnr.next().charAt(0);
         String userString = scnr.nextLine();
-        int numLetters = 0;
 
-        //read through the char and ints, count how many chars are in the user string
-        for (int i = 0; i < userString.length(); i++) {
-            if (userString.charAt(i) == userChar) {
-                numLetters += 1;
+        //read through the string
+        for (int i = 0; i < userString.length(); i++){
+            //check if string is an alpha number
+            if (Character.isLetter(userString.charAt(i))){
+                System.out.print(userString.charAt(i));
             }
-
         }
+        System.out.println();
 
-        //display output
-        if (numLetters == 1){
-            System.out.println(numLetters + " " + userChar);
-        } else {
-            System.out.println(numLetters + " " + userChar + "'s");
-        }
     }
 }
