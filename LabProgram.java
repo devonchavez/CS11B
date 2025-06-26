ab155bd3b38f993cb156196b48d2ec401154397b
Devon Chavez <ddevc415@gmail.com>
2025-06-26 02:16:50 -0700

    Lab4 Mod 4

--- LabProgram.java
+++ LabProgram.java
@@ -5,13 +5,17 @@ Instructor: Dave Harden
 Date: June 26, 2025
 File Name: LabProgram.Java
 
-This program will read string input from the user and remove and non alpha numbers from the string.
+This program will read in 2 ints from the user where each range act as bounds for a range and will display its range between the 2 numbers in increments of 5.
 
 Ex: If the input is:
--Hello, 1 world$!
-
+-15 10
 the output is:
-Helloworld
+-15 -10 -5 0 5 10
+
+Ex: If the second integer is less than the first as in:
+20 5
+the output is:
+Second integer can't be less than the first.
 */
 
 import java.util.Scanner;
@@ -19,18 +23,28 @@ import java.util.Scanner;
 public class LabProgram {
     //main method
     public static void main(String[] args) {
-        //scanner and variables
         Scanner scnr = new Scanner(System.in);
-        String userString = scnr.nextLine();
+        int num1 = scnr.nextInt();
+        int num2 = scnr.nextInt();
 
-        //read through the string
-        for (int i = 0; i < userString.length(); i++){
-            //check if string is an alpha number
-            if (Character.isLetter(userString.charAt(i))){
-                System.out.print(userString.charAt(i));
+        // if error, send msg
+        if (num1 > num2) {
+            System.out.println("Second integer can't be less than the first.");
+        } else {
+            int current = num1;
+
+            //loop while num1 is less then num2
+            while (current <= num2) {
+                System.out.print(current);
+
+                //print space
+                if (current + 5 <= num2) {
+                    System.out.print(" ");
+                }
+                current += 5;
             }
+            System.out.print(" ");
         }
-        System.out.println();
 
     }
 }
