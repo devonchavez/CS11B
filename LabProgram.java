af0b810656e314b9e4c864195878a0f4836140ee
Devon Chavez <ddevc415@gmail.com>
2025-06-30 13:23:30 -0700

    Lab1 Mod6

--- LabProgram.java
+++ LabProgram.java
@@ -2,48 +2,48 @@
 Name: Devon Chavez
 Course: CS 111B Programming Fundamentals: Java
 Instructor: Dave Harden
-Date: June 26, 2025
+Date: June 30, 2025
 File Name: LabProgram.Java
 
-The program takes in string input from the user and reverses it till they are done.
+Creating a program that switches a list of ints given from the user and swaps their positions in the array.
 
 Ex: If the input is:
-Hello there
-Hey
-done
-
-the output is:
-ereht olleH
-yeH
+3 8 2 4
+method swapValues() returns and the main program outputs:
+8 3 4 2
+The program must define and call a method:
 */
 
+import javax.crypto.spec.PSource;
 import java.util.Scanner;
 
 public class LabProgram {
+    //swap ints method
+    public static void swapValues(int[] values){
+        System.out.println(values[1] + " " + values[0] + " " + values[3] + " " + values[2]);
+    }
+
     //main method
     public static void main(String[] args) {
-        Scanner scnr = new Scanner(System.in);
-        String numString;
+        Scanner input = new Scanner(System.in);
 
-        //loop repeats till user wants to stop
-        while (true) {
-            numString = scnr.nextLine();
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int num3 = input.nextInt();
+        int num4 = input.nextInt();
 
-            //checks if user wants to quit
-            if (numString.equals("Done") || numString.equals("done") || numString.equals("d")) {
-                break;
-            }
+        //creating new array with a length of 4
+        int[] numArr = new int[4];
 
-            //print reverse string
-            for (int i = numString.length() - 1; i >= 0; i--) {
-                System.out.print(numString.charAt(i));
-            }
+        //assigning user input to arr
+        numArr[0] = num1;
+        numArr[1] = num2;
+        numArr[2] = num3;
+        numArr[3] = num4;
 
-            //newline
-            System.out.println();
-        }
+        //calling swap values
+        swapValues(numArr);
+
 
     }
 }
-
-
