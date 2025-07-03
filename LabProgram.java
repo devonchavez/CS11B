45533b8e5f5b409f716fbb94b6888a9db28d50bc
Devon Chavez <ddevc415@gmail.com>
2025-07-03 03:34:18 -0700

    Lab4 Mod5

--- LabProgram.java
+++ LabProgram.java
@@ -5,50 +5,49 @@ Instructor: Dave Harden
 Date: June 30, 2025
 File Name: LabProgram.Java
 
-
+Create a program that takes in string input from the user and puts them into an array, the program will count the frequeny ammount of times the strings appear within the users input
 
 Ex: If the input is:
-5 10 4 39 12 2
+5 hey Hi Mark hi mark
 the output is:
-39,12,10,4,2,
+hey 1
+Hi 2
+Mark 2
+hi 2
+mark 2
 */
 
 import java.util.Scanner;
 
 public class LabProgram {
-    public static void sortArray(int[] myArr, int arrSize){
-        //sort array
-        for (int i = 0; i < arrSize - 1; i++) {
-            for (int j = i + 1; j < arrSize; j++){
-                if (myArr[j] > myArr[i]){
-                    int tempVal = myArr[i];
-                    myArr[i] = myArr[j];
-                    myArr[j] = tempVal;
-                }
+    public static int getWordFrequency(String[] wordsList, int listSize, String currWord){
+        int counter = 0;
+        //count how many times a word matches the current word in the string
+        for (int i = 0; i < listSize; i++){
+            if (wordsList[i].equalsIgnoreCase(currWord)) {
+                counter++;
             }
         }
-
+        return counter;
     }
 
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int[] userArr = new int[n];
+        int userNum = input.nextInt();
+        String[] userArr = new String[userNum];
 
-        //add nums from user input into an array
+        //add all user input into an array
         for (int i = 0; i < userArr.length; i++){
-            int userNum = input.nextInt();
-            userArr[i] = userNum;
+            String userInp = input.next();
+            userArr[i] = userInp;
         }
-        //call sort array
-        sortArray(userArr, n);
 
-        //display new array
-        for (int i : userArr) {
-            System.out.print(i + ",");
+        //add each frequency to each word in array
+        for (String s : userArr) {
+            int freq = getWordFrequency(userArr, userNum, s);
+            System.out.print(s + " " + freq);
+            System.out.println();
         }
-        System.out.println();
-
     }
+
 }
