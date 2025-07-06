98e876f96bf8c493358cd4b8b8c119dddb440fcd
Devon Chavez <ddevc415@gmail.com>
2025-07-06 02:40:52 -0700

    triangle files

--- LabProgram.java
+++ LabProgram.java
@@ -6,7 +6,7 @@ Date: June 30, 2025
 File Name: LabProgram.Java
 
 Create a program that takes in string input from the user and puts them into an array, the program will count the frequeny ammount of times the strings appear within the users input
-
+/*
 Ex: If the input is:
 5 hey Hi Mark hi mark
 the output is:
@@ -20,36 +20,8 @@ mark 2
 import java.util.Scanner;
 
 public class LabProgram {
-
-    public static int fibonacci(int n){
-        int nthVal = 0;
-        int prev1 = 0;
-        int prev2 = 1;
-
-        if (n < 0){
-            return -1;
-        } else if (n == 0){
-            return 0;
-        } else if (n == 1){
-            return 1;
-        }
-
-        //swap and add sum of previous values
-        for (int i = 2; i <= n; i++) {
-            nthVal = prev1 + prev2;
-            prev1 = prev2;
-            prev2 = nthVal;
-        }
-
-        return nthVal;
-    }
-
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int userNum = input.nextInt();
 
-        //display output
-        System.out.println("fibonacci("+ userNum + ") is " + fibonacci(userNum));
 
     }
 }
---
+++ TriangleArea.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TriangleArea {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      
+      Triangle triangle1 = new Triangle();
+      Triangle triangle2 = new Triangle();
+
+      // TODO: Read and set base and height for triangle1 (use setBase() and setHeight())
+      
+      // TODO: Read and set base and height for triangle2 (use setBase() and setHeight())
+      
+      
+      System.out.println("Triangle with smaller area:");
+      
+      // TODO: Determine smaller triangle (use getArea())
+      //       and output smaller triangle's info (use printInfo())
+      
+   }
+}
