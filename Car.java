111d7e5d092816822e6d1bbfdab2cd8d23caf769
Devon Chavez <ddevc415@gmail.com>
2025-07-06 16:49:34 -0700

    finished Lab2Mod7 importing Lab3Mod7 files

--- Car.java
+++ Car.java
@@ -1,9 +1,32 @@
-public class Car {
-   private int modelYear; 
-   // TODO: Declare purchasePrice field (int)
+/*
+Name: Devon Chavez
+Course: CS 111B Programming Fundamentals: Java
+Instructor: Dave Harden
+Date: July 6, 2025
+File Name: Car.java
 
-   private int currentValue;
-   
+Given the Triangle class, complete the program that calculates input from the user, converts it to the base and hright of a triangle and print whichever triangle is the smallest along with their measurements
+
+Ex: If the input is:
+2011
+18000
+2018
+where 2011 is the car's model year, 18000 is the purchase price, and 2018 is the current year, the output is:
+
+Car's information:
+  Model year: 2011
+  Purchase price: $18000
+  Current value: $5770
+*/
+
+
+public class Car {
+   //declare private fields
+   private int modelYear;
+   private int purchasePrice;
+   public int currentValue;
+
+   //set getters for model year and purchase price
    public void setModelYear(int userYear){
       modelYear = userYear;
    }
@@ -11,21 +34,25 @@ public class Car {
    public int getModelYear() {
       return modelYear;
    }
-   
-   // TODO: Define setPurchasePrice() method
-   
-   // TODO: Define getPurchasePrice() method
-   
-   
+
+   public void setPurchasePrice(int userPrice) { purchasePrice = userPrice; }
+
+   public int getPurchasePrice() {return purchasePrice; }
+
+   //calclate the depreciation value of the cars age
    public void calcCurrentValue(int currentYear) {
       double depreciationRate = 0.15;
       int carAge = currentYear - modelYear;
       
       // Car depreciation formula
-      currentValue = (int) 
-         Math.round(purchasePrice * Math.pow((1 - depreciationRate), carAge));
+      currentValue = (int) Math.round(purchasePrice * Math.pow((1 - depreciationRate), carAge));
    }
    
-   // TODO: Define printInfo() method to output modelYear, purchasePrice, and currentValue
-   
+   //display output
+   public void printInfo(){
+      System.out.println("Car's information:");
+      System.out.println("  Model year: " + getModelYear());
+      System.out.println("  Purchase price: $" + getPurchasePrice());
+      System.out.println("  Current value: $" + currentValue);
+   }
 }
