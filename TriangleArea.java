5eb3d3be9da51cd92da9c4a049bed3212752e74d
Devon Chavez <ddevc415@gmail.com>
2025-07-06 14:18:20 -0700

    Lab1 Mod7

--- TriangleArea.java
+++ TriangleArea.java
@@ -8,14 +8,30 @@ public class TriangleArea {
       Triangle triangle2 = new Triangle();
 
       // TODO: Read and set base and height for triangle1 (use setBase() and setHeight())
-      
+      double userBase = scnr.nextDouble();
+      double userHeight = scnr.nextDouble();
+
+      triangle1.setBase(userBase);
+      triangle1.setHeight(userHeight);
+
+      double triangleOne = triangle1.getArea();
       // TODO: Read and set base and height for triangle2 (use setBase() and setHeight())
-      
+      double nextBase = scnr.nextDouble();
+      double nextHeight = scnr.nextDouble();
+
+      triangle2.setBase(nextBase);
+      triangle2.setHeight(nextHeight);
+
+      double triangleTwo = triangle2.getArea();
       
       System.out.println("Triangle with smaller area:");
       
       // TODO: Determine smaller triangle (use getArea())
       //       and output smaller triangle's info (use printInfo())
-      
+       if (triangleOne < triangleTwo) {
+          triangle1.printInfo();
+       } else {
+          triangle2.printInfo();
+       }
    }
 }
